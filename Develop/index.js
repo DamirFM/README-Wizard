4756ecb5d3095febcaf329a9d8d4f3582185b4fc
DamirFM <yakupovdamir93@gmail.com>
2023-12-06 22:18:14 -0500

    added user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,83 @@
 // TODO: Include packages needed for this application
-
+const markdowm = require('./utils/generateMarkdown')
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Add the Title, please',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Add the Description, please',
+        },
+        {
+            type: 'input',
+            name: 'content',
+            message: 'Add the Content, please',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Add the Installation, please',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Add the Usage, please',
+        },
+        {
+            type: 'checkbox',
+            message: 'Choose the License, please',
+            name: 'stack',
+            choices: ['MIT', 'Apache 2.0', 'Boost Software', 'no License']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Add the Contributing, please',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Add the Tests, please',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Add the Questions, please',
+        }
+    ])
+    .then((response) => {
+        const filename = `${response.name.toLowerCase().split(' ').join('')}.md`;
 
+        fs.writeFile(filename, 
+    `# Welcome to ${response.title}🪄
+
+    ## Description
+    ${response.description}
+    ## Content.
+    ${response.content}
+    ## Installation
+    ${response.installation}
+    ## Usage
+    ${response.usage}
+    ## License
+    ${response.license}
+    ## Contributing
+    ${response.contributing}
+    ## Tests
+    ${response.tests}
+    ## Questions
+    ${response.questions}
+    ## Badges`, (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# README-Wizard
+# Welcome to README-Wizard 🪄
+
+## Description
+
+## Content.
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
+## Badges
+
+<img src="https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)" /> 
